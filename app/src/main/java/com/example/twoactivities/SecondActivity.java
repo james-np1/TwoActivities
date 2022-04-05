028bb3a604aa55940f283f4196857a3762351e1b
James Lamichhane <ljames19@tbc.edu.np>
2022-04-06 00:17:37 +0545

    Challenge completed

--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -1,32 +1,30 @@
 package com.example.twoactivities;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
 public class SecondActivity extends AppCompatActivity {
-    public static final String EXTRA_REPLY =
-            "com.example.android.twoactivities.extra.REPLY";
-    private EditText mReply;
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message = intent.getStringExtra("button_id");
         TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-    }
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, reply);
-        setResult(RESULT_OK, replyIntent);
-        finish();
+        if (message.equals("1")) {
+            textView.setText(getResources().getString(R.string.TextOneMessage));
+
+        } else if (message.equals("2")) {
+            textView.setText(getResources().getString(R.string.TextTwoMessage));
+
+        } else if (message.equals("3")) {
+            textView.setText(getResources().getString(R.string.TextThreeMessage));
+        }
     }
 }
